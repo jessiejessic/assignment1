c57db2d524e94ed5cb793efc135216014ad51bb5
jessiejessic <xacka.dangol@gmail.com>
2022-01-10 16:33:45 +0545

    feature 1

---
+++ age.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+int main()
+{
+    int age;
+    printf("please enter you age:");
+    scanf("%d",&age);
+    if(age>65)
+        printf("Yes you are eligible ");
+    else
+        printf("sorry!! you're not yet eligible .");
+    return 0;
+}
